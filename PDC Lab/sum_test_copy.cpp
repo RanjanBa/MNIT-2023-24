cd26f0b57613f51755986dfe211f145e7dddf5a3
RanjanBa <ranjanbaro2015@gmail.com>
2023-10-01 01:35:15 +0530

    histogram equalization

--- PDC Lab/sum_test_copy.cpp
+++ PDC Lab/sum_test_copy.cpp
@@ -2,27 +2,22 @@
 #include <stdio.h>
 #include <omp.h>
 
+#define Arr_Size 1000000000
+
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    long long mxN = 1000000000;
-    long long sum = 0;
-    int t_num = 0;
-
     double t1 = omp_get_wtime();
 
-#pragma omp parallel
-    t_num = omp_get_num_threads();
-
-    long dis_size = mxN / t_num;
+    long long sum = 0;
 
 #pragma omp parallel
     {
         long long psum = 0;
-        int t_id = omp_get_thread_num();
 
-        for (size_t i = t_id * dis_size; i < t_id * dis_size + dis_size; i++)
+#pragma omp for
+        for (size_t i = 0; i < Arr_Size; i++)
         {
             psum++;
         }
@@ -34,7 +29,7 @@ int main(int argc, char const *argv[])
 
     double t2 = omp_get_wtime();
 
-    printf("Sum : %d , time : %f", sum, (t2 - t1));
+    printf("Sum : %d , time: %f\n", sum, (t2 - t1));
 
     return 0;
 }
