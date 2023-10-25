64914fafe07f57f395361ab82085af52bb65e4a3
RanjanBa <ranjanbaro2015@gmail.com>
2023-10-25 16:18:30 +0530

    pdc assignment completed

--- PDC Lab/pdc_assign_2.cpp
+++ PDC Lab/pdc_assign_2.cpp
@@ -7,13 +7,14 @@ int main(int argc, char const *argv[])
 {
     int thread_id, threads_num;
 
+    omp_set_num_threads(3);
+
     #pragma omp parallel private(thread_id) shared(threads_num)
     {
         thread_id = omp_get_thread_num();
         threads_num = omp_get_num_threads();
 
         printf("id : %d , total threads : %d\n", thread_id, threads_num);
-        //cout << "id : " << thread_id << " , total threads : " << threads_num << "\n";
     }
 
     return 0;
